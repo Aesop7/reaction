80ebc8943b6532434f87668dedba0cc6371fd3be
artie <artie@creativewebvermont.com>
2016-04-26 20:17:30 -0400

    try to fix issues with template replacmements

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -14,8 +14,8 @@
  Template.indieProducts.replaces("products");
  Template.indieProductListGrid.replaces("productListGrid");
  Template.indieHeaderBrand.replaces("headerBrand");
- Template.indieProductDetail.replaces("productDetail");
- Template.indieProductDetail.replaces("productDetailReact");
+ Template.customPDP.replaces("productDetail");
+ //Template.indieProductDetail.replaces("productDetailReact");
  //Template.indieSignUpForm.replaces "loginInLine"
  //Template.indieCartCheckout.replaces "cartCheckout"
  //Template.checkoutAddressBookShim.replaces "checkoutAddressBookShim"
