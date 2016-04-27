031006c84c8a5915d89464e4ec59fd9816cd80d5
artie <artie@creativewebvermont.com>
2016-04-26 20:00:43 -0400

    add template replacements and fix gitignore for packages

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -9,3 +9,13 @@
 
 
 // Template.layout.replaces("coreLayout");
+ Template.layout.replaces("coreLayout");
+ Template.layoutHeaderCustom.replaces("layoutHeader");
+ Template.indieProducts.replaces("products");
+ Template.indieProductListGrid.replaces("productListGrid");
+ Template.indieHeaderBrand.replaces("headerBrand");
+ Template.indieProductDetail.replaces("productDetail");
+ Template.indieProductDetail.replaces("productDetailReact");
+ //Template.indieSignUpForm.replaces "loginInLine"
+ //Template.indieCartCheckout.replaces "cartCheckout"
+ //Template.checkoutAddressBookShim.replaces "checkoutAddressBookShim"
