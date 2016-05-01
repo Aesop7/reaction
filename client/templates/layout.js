9f8e58e65b2e6df9d7c9a30ca07c3d817af74e38
artie <artie@creativewebvermont.com>
2016-05-01 16:06:16 -0400

    update the layout

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -9,13 +9,9 @@
 
 
 // Template.layout.replaces("coreLayout");
- Template.layout.replaces("coreLayout");
- //Template.layoutHeaderCustom.replaces("layoutHeader");
- Template.indieProducts.replaces("products");
- Template.indieProductListGrid.replaces("productListGrid");
- Template.indieHeaderBrand.replaces("headerBrand");
- Template.customPDP.replaces("productDetail");
- //Template.indieProductDetail.replaces("productDetailReact");
- //Template.indieSignUpForm.replaces "loginInLine"
- //Template.indieCartCheckout.replaces "cartCheckout"
- //Template.checkoutAddressBookShim.replaces "checkoutAddressBookShim"
+Template.layout.replaces("coreLayout");
+Template.indieLayoutHeader.replaces("layoutHeader");
+Template.indieCoreNavigationBar.replaces("CoreNavigationBar");
+Template.indieProducts.replaces("products");
+Template.indieHeaderBrand.replaces("headerBrand");
+Template.customPDP.replaces("productDetail");
