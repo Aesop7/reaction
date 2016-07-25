f6d87b0609c43bfe41d58d5b3a62e1a55797db9c
aesop7 <aflegenheimer1985@gmail.com>
2016-07-24 21:25:54 -0400

    Try removing the function itself?

--- server/methods/core/shop.js
+++ server/methods/core/shop.js
@@ -62,88 +62,88 @@ Meteor.methods({
    * determine local currency and conversion rate from shop currency
    * @return {Object} returns user location and locale
    */
-  "shop/getLocale": function () {
-    this.unblock();
-    let clientAddress;
-    let geo = new GeoCoder();
-    let result = {};
-    let defaultCountryCode = "US";
-    let localeCurrency = "USD";
-    // if called from server, ip won't be defined.
-    if (this.connection !== null) {
-      clientAddress = this.connection.clientAddress;
-    } else {
-      clientAddress = "127.0.0.1";
-    }
-
-    // get shop locale/currency related data
-    let shop = Collections.Shops.findOne(Reaction.getShopId(), {
-      fields: {
-        addressBook: 1,
-        locales: 1,
-        currencies: 1,
-        currency: 1
-      }
-    });
-
-    if (!shop) {
-      throw new Meteor.Error(
-        "Failed to find shop data. Unable to determine locale.");
-    }
-    // cofigure default defaultCountryCode
-    // fallback to shop settings
-    if (shop.addressBook) {
-      if (shop.addressBook.length >= 1) {
-        if (shop.addressBook[0].country) {
-          defaultCountryCode = shop.addressBook[0].country;
-        }
-      }
-    }
-    // geocode reverse ip lookup
-    let geoCountryCode = geo.geoip(clientAddress).country_code;
-
-    // countryCode either from geo or defaults
-    let countryCode = (geoCountryCode || defaultCountryCode).toUpperCase();
-
-    // get currency rates
-    result.currency = {};
-    result.locale = shop.locales.countries[countryCode];
-
-    // to return default currency if rates will failed, we need to bring access
-    // to this data
-    result.shopCurrency = shop.currencies[shop.currency];
-
-    // check if locale has a currency defined
-    if (typeof result.locale === "object" &&
-      typeof result.locale.currency === "string") {
-      localeCurrency = result.locale.currency.split(",");
-    }
-
-    // localeCurrency is an array of allowed currencies
-    _.each(localeCurrency, function (currency) {
-      let exchangeRate;
-      if (shop.currencies[currency]) {
-        result.currency = shop.currencies[currency];
-        // only fetch rates if locale and shop currency are not equal
-        // if shop.curency = locale currency the rate is 1
-        if (shop.currency !== currency) {
-          exchangeRate = Meteor.call("shop/getCurrencyRates", currency);
-
-          if (typeof exchangeRate === "number") {
-            result.currency.exchangeRate = exchangeRate;
-          } else {
-            Logger.warn("Failed to get currency exchange rates.");
-          }
-        }
-      }
-    });
-
-    // set server side locale
-    Reaction.Locale = result;
-
-    // should contain rates, locale, currency
-    return result;
-  },
+  // "shop/getLocale": function () {
+  //   this.unblock();
+  //   let clientAddress;
+  //   let geo = new GeoCoder();
+  //   let result = {};
+  //   let defaultCountryCode = "US";
+  //   let localeCurrency = "USD";
+  //   // if called from server, ip won't be defined.
+  //   if (this.connection !== null) {
+  //     clientAddress = this.connection.clientAddress;
+  //   } else {
+  //     clientAddress = "127.0.0.1";
+  //   }
+  //
+  //   // get shop locale/currency related data
+  //   let shop = Collections.Shops.findOne(Reaction.getShopId(), {
+  //     fields: {
+  //       addressBook: 1,
+  //       locales: 1,
+  //       currencies: 1,
+  //       currency: 1
+  //     }
+  //   });
+  //
+  //   if (!shop) {
+  //     throw new Meteor.Error(
+  //       "Failed to find shop data. Unable to determine locale.");
+  //   }
+  //   // cofigure default defaultCountryCode
+  //   // fallback to shop settings
+  //   if (shop.addressBook) {
+  //     if (shop.addressBook.length >= 1) {
+  //       if (shop.addressBook[0].country) {
+  //         defaultCountryCode = shop.addressBook[0].country;
+  //       }
+  //     }
+  //   }
+  //   // geocode reverse ip lookup
+  //   let geoCountryCode = geo.geoip(clientAddress).country_code;
+  //
+  //   // countryCode either from geo or defaults
+  //   let countryCode = (geoCountryCode || defaultCountryCode).toUpperCase();
+  //
+  //   // get currency rates
+  //   result.currency = {};
+  //   result.locale = shop.locales.countries[countryCode];
+  //
+  //   // to return default currency if rates will failed, we need to bring access
+  //   // to this data
+  //   result.shopCurrency = shop.currencies[shop.currency];
+  //
+  //   // check if locale has a currency defined
+  //   if (typeof result.locale === "object" &&
+  //     typeof result.locale.currency === "string") {
+  //     localeCurrency = result.locale.currency.split(",");
+  //   }
+  //
+  //   // localeCurrency is an array of allowed currencies
+  //   _.each(localeCurrency, function (currency) {
+  //     let exchangeRate;
+  //     if (shop.currencies[currency]) {
+  //       result.currency = shop.currencies[currency];
+  //       // only fetch rates if locale and shop currency are not equal
+  //       // if shop.curency = locale currency the rate is 1
+  //       if (shop.currency !== currency) {
+  //         exchangeRate = Meteor.call("shop/getCurrencyRates", currency);
+  //
+  //         if (typeof exchangeRate === "number") {
+  //           result.currency.exchangeRate = exchangeRate;
+  //         } else {
+  //           Logger.warn("Failed to get currency exchange rates.");
+  //         }
+  //       }
+  //     }
+  //   });
+  //
+  //   // set server side locale
+  //   Reaction.Locale = result;
+  //
+  //   // should contain rates, locale, currency
+  //   return result;
+  // },
 
   /**
    * shop/getCurrencyRates
