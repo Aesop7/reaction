01ff66d3435cd56c889483f1378d2c533639edec
Ryan Mannigan <laxgoalie392@gmail.com>
2016-08-08 21:15:54 -0400

    small tweaks for reminders

--- .reaction/scripts/postinstall.sh
+++ .reaction/scripts/postinstall.sh
@@ -9,3 +9,6 @@ cp -R node_modules/font-awesome/fonts ./public/
 
 # setup plugin imports on client and server
 bash .reaction/docker/scripts/plugin-loader.sh
+
+# copy custom plugins public dir
+cp -R imports/plugins/custom/custom-reminders/public/** ./public/custom
